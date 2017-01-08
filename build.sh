8f0c7a90cfc3e96e92709d4332af8ef05d21acfc
Matt Snoby <snobym@cisco.com>
2017-01-08 07:07:05 -0500

    removed proxy and updated image from latest jenkins:alpine image

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-export http_proxy=http://10.0.0.131:3128
+#export http_proxy=http://10.0.0.131:3128
 #docker build --build-arg HTTP_PROXY=http://1.0.0.105:3128 -t jenkins .
 docker build --build-arg=HTTP_PROXY=$http_proxy -t iotapi322/jenkins_master .
